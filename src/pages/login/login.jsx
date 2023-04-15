7cd005a3f5d7a970385d4065974c7bd5632db08f
nourahamed111 <nourahamed111@gmail.com>
2023-04-15 13:45:58 +0200

    validation added

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import "../../styles/login.scss";
 import Logo from "../../components/logo";
 
@@ -6,7 +7,19 @@ export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   const handlePasswordVisibility = () => {
-    setShowPassword(!showPassword);
+    if (!errors.password) {
+      setShowPassword(!showPassword);
+    }
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
@@ -14,36 +27,71 @@ export default function Login() {
       <div className="col-12 d-flex justify-content-center align-items-center outer-div">
         <div className="main-div">
           <Logo />
-          <div className="login-form">
+          <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
             <h1>Login</h1>
-            <div className="form-floating ">
+            <div className="form-floating">
               <input
                 type="email"
-                className="form-control"
+                className={`form-control ${errors.email ? "is-invalid" : ""}`}
                 id="floatingInput"
                 placeholder="name@example.com"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
               />
               <label htmlFor="floatingInput">Email address</label>
+              {errors.email && (
+                <div className="invalid-feedback">{errors.email.message}</div>
+              )}
             </div>
             <div className="form-floating password-input">
               <input
                 type={showPassword ? "text" : "password"}
-                className="form-control"
+                className={`form-control ${
+                  errors.password ? "is-invalid" : ""
+                }`}
                 placeholder="Password"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must have at least 8 characters",
+                  },
+                  pattern: {
+                    value: /^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/,
+                    message:
+                      "Password must contain at least 1 number and 1 letter",
+                  },
+                })}
               />
               <label htmlFor="floatingInput">Password</label>
-              <img
-               alt="password"
-                className="password-icon"
-                onClick={handlePasswordVisibility}
-                src="https://img.icons8.com/material-sharp/24/1A1A1A/visible.png"
-              />
+              {!errors.password && (
+                <img
+                  alt="password"
+                  className="password-icon"
+                  onClick={handlePasswordVisibility}
+                  src={
+                    showPassword
+                      ? "https://img.icons8.com/material/24/1A1A1A/invisible.png"
+                      : "https://img.icons8.com/material-sharp/24/1A1A1A/visible.png"
+                  }
+                />
+              )}
+              {errors.password && (
+                <div className="invalid-feedback">
+                  {errors.password.message}
+                </div>
+              )}
             </div>
-          </div>
-          <div className="form-actions">
-            <h6>Forgot my password</h6>
-            <button>LOGIN</button>
-          </div>
+            <div className="form-actions">
+              <h6>Forgot my password</h6>
+              <button type="submit">LOGIN</button>
+            </div>
+          </form>
           <div className="break-div">
             <hr />
             <p>or</p>
@@ -51,11 +99,17 @@ export default function Login() {
           </div>
           <div className="login-options">
             <button>
-              <img alt="icon"  src="https://img.icons8.com/color/48/null/google-logo.png" />
+              <img
+                alt="icon"
+                src="https://img.icons8.com/color/48/null/google-logo.png"
+              />
               Sign in With Google
             </button>
             <button>
-              <img alt="icon" src="https://img.icons8.com/color/48/null/microsoft.png" />
+              <img
+                alt="icon"
+                src="https://img.icons8.com/color/48/null/microsoft.png"
+              />
               Sign in With Microsoft
             </button>
           </div>
--- src/pages/otp/otp.jsx
+++ src/pages/otp/otp.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import '../../styles/otp.scss';
+import { useForm } from "react-hook-form";
+import "../../styles/otp.scss";
 import Logo from "../../components/logo";
+
 export default function Otp() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    console.log("Verifying OTP: ", data);
+  };
   return (
     <>
       <div className="d-flex justify-content-center align-items-center">
         <div className="main-div">
           <Logo />
           <div className="headers">
-            <h2> Verify Your Identity</h2>
+            <h2>Verify Your Identity</h2>
             <h5>
               We've sent a code to your configured two-factor authentication
               method
@@ -19,48 +29,62 @@ export default function Otp() {
               type="text"
               id="otp-input-1"
               className="otp-input"
-              inputmode="numeric"
-              maxlength="1"
+              inputMode="numeric"
+              maxLength="1"
+              {...register("otp1", { required: true })}
             />
             <input
               type="text"
               id="otp-input-2"
               className="otp-input"
-              inputmode="numeric"
-              maxlength="1"
+              inputMode="numeric"
+              maxLength="1"
+              {...register("otp2", { required: true })}
             />
             <input
               type="text"
               id="otp-input-3"
               className="otp-input"
-              inputmode="numeric"
-              maxlength="1"
+              inputMode="numeric"
+              maxLength="1"
+              {...register("otp3", { required: true })}
             />
             <input
               type="text"
               id="otp-input-4"
               className="otp-input"
-              inputmode="numeric"
-              maxlength="1"
+              inputMode="numeric"
+              maxLength="1"
+              {...register("otp4", { required: true })}
             />
             <input
               type="text"
               id="otp-input-5"
               className="otp-input"
-              inputmode="numeric"
-              maxlength="1"
+              inputMode="numeric"
+              maxLength="1"
+              {...register("otp5", { required: true })}
             />
             <input
               type="text"
               id="otp-input-6"
               className="otp-input"
-              inputmode="numeric"
-              maxlength="1"
+              inputMode="numeric"
+              maxLength="1"
+              {...register("otp6", { required: true })}
             />
           </div>
+          {errors.otp1 ||
+            errors.otp2 ||
+            errors.otp3 ||
+            errors.otp4 ||
+            errors.otp5 ||
+            errors.otp6 ? (
+            <p className="error-msg">Please enter all 6 digits</p>
+          ) : null}
           <div className="form-actions">
             <h6>Send code again</h6>
-            <button>CONTINUE</button>
+            <button onClick={handleSubmit(onSubmit)}>CONTINUE</button>
           </div>
         </div>
       </div>
